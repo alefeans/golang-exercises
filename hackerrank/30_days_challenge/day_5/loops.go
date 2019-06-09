3aa88c1ffdcd389ecef7c4233a7218fa3a9377db
alefeans <alefeans2@hotmail.com>
2019-06-08 21:00:55 -0300

    loops solution, still easy yet

---
+++ hackerrank/30_days_challenge/day_5/loops.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	multiply(int(nTemp))
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func multiply(n int) {
+	for i := 1; i < 11; i++ {
+		fmt.Println(n, "x", i, "=", n*i)
+	}
+}
